7878fa05f2a9f48cba4ee179561947b8b686259a
zhaonan <zhaonan@fiture.com>
2021-04-21 17:57:13 +0800

    feat: :sparkles: integration markdown editor & preview

--- src/pages/article/add-article/index.tsx
+++ src/pages/article/add-article/index.tsx
@@ -1,7 +1,25 @@
 import * as React from 'react'
+import gfm from 'remark-gfm'
+import MarkdownIt from 'markdown-it'
+import MdEditor from 'react-markdown-editor-lite'
+import 'react-markdown-editor-lite/lib/index.css'
 
 const AddArticles = () => {
-  return <div>AddArticles</div>
+  function handleEditorChange({ html, text }) {
+    console.log('handleEditorChange', html, text)
+  }
+  const mdParser = new MarkdownIt(/* Markdown-it options */)
+
+  return (
+    <>
+      <div>addArticles</div>
+      <MdEditor
+        style={{ height: '500px' }}
+        renderHTML={(text) => mdParser.render(text)}
+        onChange={handleEditorChange}
+      />
+    </>
+  )
 }
 
 export default AddArticles
