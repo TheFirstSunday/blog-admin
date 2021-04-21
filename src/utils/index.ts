5306372b8a72f3f96f1b3ce60af2b683acb930ba
zhaonan <zhaonan@fiture.com>
2021-04-21 18:44:31 +0800

    feat: :sparkles: add copy util into utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,16 @@
 export function isFunction(obj: any): obj is Function {
-  return typeof obj === 'function';
+  return typeof obj === 'function'
+}
+
+export function copy(text: string, callback: () => any) {
+  const textArea = document.createElement('textarea')
+
+  textArea.style.position = 'fixed'
+  textArea.style.opacity = '0'
+  textArea.value = text
+  document.body.appendChild(textArea)
+  textArea.select()
+  document.execCommand('copy')
+  document.body.removeChild(textArea)
+  callback?.()
 }
